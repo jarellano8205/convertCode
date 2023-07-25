3e21adebf51fd0bc8d6ba54c70e1aaeb1c7abadd
Johnny <jarellano8205@gmail.com>
2023-07-24 19:20:23 -0600

    added packetizeTemp function, need to get it compiling/running

--- main.cpp
+++ main.cpp
@@ -2,11 +2,32 @@
 
 int main(int argc, char* argv[])
 {
-    string binaryNum = "0100001100011010001000000000100";
+    /*
+    * binary to float test
+    string binaryNum = "0100000000011010001000000000100";
 
     float result = bin2float(binaryNum);
 
     cout << "float val: " << result << endl;
+    */
 
+    vector<bool> transmitMessage = { true, false, true, true };
+
+    char charData = 'A';
+
+    transmitMessage = packetizeTemp(transmitMessage, charData);
+
+    uint32_t uintData = 123456789;
+    transmitMessage = packetizeTemp(transmitMessage, uintData);
+
+    float floatData = 3.14f;
+    transmitMessage = packetizeTemp(transmitMessage, floatData);
+
+    for (bool bit : transmitMessage)
+    {
+        cout << bit;
+    }
+
+    cout << endl;
     return 0;
 }
--- support.h
+++ support.h
@@ -4,13 +4,21 @@
 #include <iostream>
 #include <string>
 #include <cmath>
+#include <vector>
+#include <bitset>
 
 using std::cout;
 using std::endl;
 using std::string;
 using std::pow;
 using std::stoi;
+using std::vector;
+using std::is_same_v;
+using std::bitset;
 
 float bin2float(const string& binary);
+
+template<typename T>
+vector<bool> packetizeTemp(const vector<bool>& transmitMessage, const T& message);
 
 #endif
